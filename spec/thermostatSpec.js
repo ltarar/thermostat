2b0aeba7cbee281d95603ae120fa54fb9340d9ac
Wardah <wlt_93@hotmail.com>
2019-04-03 15:02:04 +0100

    Decreases temperature with down function. Passes test

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,18 +1,28 @@
 'use strict'
 
 describe('Thermostat', function() {
+  // var thermostat = new Thermostat()
+  var thermostat;
 
-  var thermostat = new Thermostat()
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
 
   describe('starts at 20 degrees', function() {
     it('20 degrees', function() {
       expect(thermostat.getCurrentTemperature()).toEqual(20)
     })
 
-    it('increases temperature with up()', function() {
+    it('increases in temperature with up()', function() {
       thermostat.up()
       expect(thermostat.getCurrentTemperature()).toEqual(21) 
     })
+
+    it('decreases in temperature with down()', function() {
+      thermostat.down()
+      expect(thermostat.getCurrentTemperature()).toEqual(19)
+    })
+
   })
 
 })
--- src/thermostat.js
+++ src/thermostat.js
@@ -8,6 +8,10 @@ Thermostat.prototype.getCurrentTemperature = function () {
   return this.temperature
 }
 
-Thermostat.prototype.up = function (){
+Thermostat.prototype.up = function () {
   this.temperature += 1
+}
+
+Thermostat.prototype.down = function() {
+  this.temperature -= 1
 }
